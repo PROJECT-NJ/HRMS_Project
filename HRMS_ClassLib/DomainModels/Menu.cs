95a9b9958479db8330344962c78fc21ffb315bd3
Natdanai <lskraken@hotmail.com>
2020-07-05 16:19:49 +0700

    Manage Role & Menu

--- HRMS_ClassLib/DomainModels/Menu.cs
+++ HRMS_ClassLib/DomainModels/Menu.cs
@@ -1,22 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace HRMS_WebApi.Models
+namespace HRMS_ClassLib.DomainModels
 {
     public class Menu
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string MenuID { get; set; }
         public string MenuGroup { get; set; }
         public string MenuNameEng { get; set; }
         public string MenuNameTh { get; set; }
-        public string ControllerID { get; set; }
+        public string Controller { get; set; }
+        public string Action { get; set; }
         public string CreateBy { get; set; }
         public DateTime CreateDate { get; set; }
         public string UpdateBy { get; set; }
         public DateTime UpdateDate { get; set; }
-
         public IList<RoleMenu> RoleMenu { get; set; }
     }
 }
--- HRMS_ClassLib/DomainModels/Role.cs
+++ HRMS_ClassLib/DomainModels/Role.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace HRMS_WebApi.Models
+namespace HRMS_ClassLib.DomainModels
 {
     public class Role
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string RoleId { get; set; }
         public string RoleName { get; set; }
         public string CreateBy { get; set; }
         public DateTime CreateDate { get; set; }
         public string UpdateBy { get; set; }
         public DateTime UpdateDate { get; set; }
-
         public IList<RoleMenu> RoleMenu { get; set; }
     }
 }
